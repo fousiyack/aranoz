f089f992b11b2c029920c5710266197a1951f17c
fousiyack <90838495+fousiyack@users.noreply.github.com>
2023-03-14 09:47:39 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -171,5 +171,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='letterforfousi@gmail.com'
-EMAIL_HOST_PASSWORD='aphrqjkwlotjcetu'
+EMAIL_HOST_PASSWORD='#'
 EMAIL_USE_TLS=True
