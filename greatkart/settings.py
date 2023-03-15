e3200ea88ec1aec6991370eb5b9a86feb5456ce9
fousiyack <fousiyack64@gmail.com>
2023-03-15 11:39:23 +0530

    first commit

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*sl_6i!+&duvq%^w_9zaf2gzc=))i+#)nne9z7t4bmerm+0do5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
